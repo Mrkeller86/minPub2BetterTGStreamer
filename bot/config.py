620690753afaba19c9e52ac1992adc6b8170fff3
Ravandeadlya <Ravandeadly@outlook.com>
2024-10-04 12:53:19 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,48 +1,36 @@
-# Telegram API ID and Hash
-API_ID = int("telegram_api_id")
-API_HASH = "telegram_api_hash"
+#by back4app.com
+import os
+print(os.environ)  # Print all environment variables
+print(os.getenv('bot_token1'))  # Print the value of bot_token1
 
-# Bot which will handle users commands
-MAIN_BOT_TOKEN = "main_bot_token"
+# Fetch individual tokens from environment variables
+bot_token1 = os.getenv('bot_token1', 'default_bot_token1')
+bot_token2 = os.getenv('bot_token2', 'default_bot_token2')
+bot_token3 = os.getenv('bot_token3', 'default_bot_token3')
+bot_token4 = os.getenv('bot_token4', 'default_bot_token4')
+bot_token5 = os.getenv('bot_token5', 'default_bot_token5')
+bot_token6 = os.getenv('bot_token6', 'default_bot_token6')
 
-LOGGER_BOT_TOKEN = "logger_bot_token"
+# Construct lists from individual environment variables
+UPLOADER_BOTS_1 = [bot_token1, bot_token2, bot_token3]
+UPLOADER_BOTS_2 = [bot_token4, bot_token5, bot_token6]
 
-# Bot tokens for STORAGE_CHANNEL_1, they all must be admin in the channel
-UPLOADER_BOTS_1 = ["bot_token1", "bot_token2", "bot_token3"]
-
-# Bot tokens for STORAGE_CHANNEL_2, they all must be admin in the channel
-UPLOADER_BOTS_2 = ["bot_token4", "bot_token5", "bot_token6"]
-
-# Telegram Channel ID where videos will be stored
-VIDEO_STORAGE = -1001234567890
-# Telegram Channel ID where logs will be stored
-LOGGER_CHANNEL = -1001234567890
-# Telegram Channel ID where m3u8 file will be stored
-STORAGE_CHANNEL_1 = -1001234567890
-STORAGE_CHANNEL_2 = -1001234567890
-
-# Max no. of tasks to run simultaneously
-MAX_ACTIVE_TASKS = 5
-
-# Max no. of tasks a user can run simultaneously
-MAX_USER_CONCURRENT_TASKS = 3
-
-# No. of simulataneous uploaders to run per task for .ts file upload
-NO_OF_UPLOADERS = 5
-
-# In MB (MegaBytes). Size of each segment uploaded to telegram. Must be less than 10 MB
-SEGMENT_SIZE = 2
-
-# Telegram User ID of the bot owner
-OWNER_ID = 1234567890
-
-# Domain name of the website where the stream api is hosted
-WEBSITE_DOMAIN = "https://stream.mywebsite.com"
-
-# MongoDB Database URL
-MONGO_DB_URL = "YOUR_MONGODB_URL"
-
-# Playerx.stream Login details
-PLAYERX_EMAIL = "your_email@gmail.com"
-PLAYERX_PASSWORD = "your_password"
-PLAYERX_API_KEY = "your_api"
+# Other configurations
+API_ID = os.getenv('API_ID', 'default_api_id')
+API_HASH = os.getenv('API_HASH', 'default_api_hash')
+MAIN_BOT_TOKEN = os.getenv('MAIN_BOT_TOKEN', 'default_main_bot_token')
+LOGGER_BOT_TOKEN = os.getenv('LOGGER_BOT_TOKEN', 'default_logger_bot_token')
+MONGO_DB_URL = os.getenv('MONGO_DB_URL', 'default_mongo_db_url')
+WEBSITE_DOMAIN = os.getenv('WEBSITE_DOMAIN', 'default_website_domain')
+PLAYERX_API_KEY = os.getenv('PLAYERX_API_KEY', 'default_playerx_api_key')
+PLAYERX_EMAIL = os.getenv('PLAYERX_EMAIL', 'default_playerx_email')
+PLAYERX_PASSWORD = os.getenv('PLAYERX_PASSWORD', 'default_playerx_password')
+VIDEO_STORAGE = os.getenv('VIDEO_STORAGE', 'default_VIDEO_STORAGE')
+LOGGER_CHANNEL = os.getenv('LOGGER_CHANNEL', 'default_LOGGER_CHANNEL')
+STORAGE_CHANNEL_1 = os.getenv('STORAGE_CHANNEL_1', 'default_STORAGE_CHANNEL_1')
+STORAGE_CHANNEL_2 = os.getenv('STORAGE_CHANNEL_2', 'default_STORAGE_CHANNEL_2')
+MAX_ACTIVE_TASKS = os.getenv('MAX_ACTIVE_TASKS', 'default_MAX_ACTIVE_TASKS')
+MAX_USER_CONCURRENT_TASKS = os.getenv('MAX_USER_CONCURRENT_TASKS', 'default_MAX_USER_CONCURRENT_TASKS')
+NO_OF_UPLOADERS = os.getenv('NO_OF_UPLOADERS', 'default_NO_OF_UPLOADERS')
+SEGMENT_SIZE = os.getenv('SEGMENT_SIZE', 'default_SEGMENT_SIZE')
+OWNER_ID = os.getenv('OWNER_ID', 'default_OWNER_ID')
